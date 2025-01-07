8755769cca5aefdbe4593d714e4d3ca77160e8ec
Vinamra <vinamra2013@gmail.com>
2025-01-07 10:54:05 -0600

    run

--- run.sh
+++ run.sh
@@ -5,19 +5,31 @@ export PATH="/opt/blender:$PATH"
 
 # Install necessary dependencies
 echo "Updating and installing required packages..."
-apt-get update && \
-    apt-get install -y software-properties-common && \
-    add-apt-repository -y ppa:deadsnakes/ppa && \
-    apt-get update && \
-    apt-get install -y python3.11 python3.11-distutils curl tzdata libxi6 libgconf-2-4 libfontconfig1 libxrender1 libboost-all-dev libgl1-mesa-dev libglu1-mesa libsm-dev libxkbcommon-x11-dev && \
-    curl -sS https://bootstrap.pypa.io/get-pip.py | python3.11 && \
-    apt-get clean && \
-    rm -rf /var/lib/apt/lists/*
+# apt-get update && \
+#     apt-get install -y software-properties-common && \
+#     add-apt-repository -y ppa:deadsnakes/ppa && \
+#     apt-get update && \
+#     apt-get install -y python3.11 python3.11-distutils curl tzdata libxi6 libgconf-2-4 libfontconfig1 libxrender1 libboost-all-dev libgl1-mesa-dev libglu1-mesa libsm-dev libxkbcommon-x11-dev && \
+#     curl -sS https://bootstrap.pypa.io/get-pip.py | python3.11 && \
+#     apt-get clean && \
+#     rm -rf /var/lib/apt/lists/*
 
-# Create symbolic links for default Python and pip
-ln -sf /usr/bin/python3.11 /usr/bin/python
-ln -sf /usr/bin/python3.11 /usr/bin/python3
-ln -sf /usr/local/bin/pip /usr/bin/pip
+!apt update -y
+# !apt upgrade -y
+!apt-get install curl libxi6 libgconf-2-4 -y
+!apt install libfontconfig1 libxrender1 -y
+!apt install libboost-all-dev -y
+!apt install libgl1-mesa-dev -y
+!apt install libglu1-mesa libsm-dev libxkbcommon-x11-dev -y
+!apt update -y
+# !apt upgrade -y
+
+
+
+# # Create symbolic links for default Python and pip
+# ln -sf /usr/bin/python3.11 /usr/bin/python
+# ln -sf /usr/bin/python3.11 /usr/bin/python3
+# ln -sf /usr/local/bin/pip /usr/bin/pip
 
 # Set Blender version and download URL
 BLENDER_VERSION=4.2.2
